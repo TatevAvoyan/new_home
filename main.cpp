458b8fb1784aeba2d2574292b8bc73b8ad494247
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-16 23:43:07 +0400

    overload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,23 @@
 #include "iostream"
 using namespace std;
 
-void print_hello(){
-
-    char hello;
-    std::cin >> hello;
-
-    switch (hello) {
-        case 'h' : case 'y' : std::cout << "Barev"; break;
-        case 'r' : case 'u' : std::cout << "Privet"; break;
-        case 'e' : case 'n' : std::cout << "Hello"; break;
-        default : std::cout << "Hi";
+class Test{
+    int num1;
+public:
+    Test(int n1){
+        this->num1 = n1;
     }
+    Test operator + (const Test){
+        this->num1--;
+    }
+};
 
-    /* if ( w == "hy"){
-         std::cout << "Barev";
-     } else if (w == "ru"){
-         std::cout << "Privet";
-     } else if (w == "en"){
-         std::cout << "Hello";
-     } else{
-         std::cout << "Hi";
-     }*/
 
-}
 int main(){
 
-    print_hello();
+    Test test(5);
+    test.operator+(test);
+
 
     return 0;
 }
