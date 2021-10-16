93b725da45428590ff75240164657583f506f863
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-16 23:56:25 +0400

    Triangle

--- main.cpp
+++ main.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 void print_triangle(int rows){
-    for(int i = rows; i >= 1; --i)
-    {
-        for(int space = 0; space < rows-i; space++)
-            cout << "  ";
+    for(int i = rows; i >= 1; i--){
 
-        for(int j = i; j <= 2*i-1; j++)
+        for(int space = 0; space < rows-i; space++){
+            cout << " ";
+        }
+        for(int j = i; j <= 2*i-1; j++) {
             cout << "* ";
+        }
 
-        for(int j = 0; j < i-1; j++)
-            cout << "* ";
         cout << endl;
     }
 }
 int main(){
 
-    print_triangle(5);
+    print_triangle(7);
 
     return 0;
 }
