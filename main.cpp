df3a9591c182ceffb6abcdc4e48ba78f165b82c2
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-21 14:51:07 +0400

    replace word

--- main.cpp
+++ main.cpp
@@ -1,31 +1,48 @@
 #include "iostream"
-using namespace std;
+#include "string"
 
-class Test {
-    int num;
-public:
-    Test(int n){
-        this->num = n;
-    }
-    void operator ++(){
-        num*=num;
-    }
-    void operator --(){
-        ++num;
-    }
-    void print_num(){
-        cout << "num = " << num << endl;
-    }
-};
 
-int main()
-{
-    Test test(10);
-    test.print_num();
-    ++test;
-    test.print_num();
-    --test;
-    test.print_num();
+void replace_dummy(){
+
+    std::cout << "Enter the text.\n";
+    std::string text;
+    std::getline(std::cin, text);
+    auto word = text.find("dummy");
+
+    std::cout << word << "\n";
+
+    if (std::string::npos == word ) {
+        std::cout << "not found\n";
+    } else {
+        text.replace(word + 4, 1, "i");
+    }
+
+    std::cout << text << std::endl;
+}
+
+void replace_is(){
+
+    std::cout << "Enter the text.\n";
+    std::string text;
+    std::getline(std::cin, text);
+    auto word = text.find("is");
+
+    std::cout << word << "\n";
+
+    if (std::string::npos == word ) {
+        std::cout << "not found\n";
+    } else {
+        text.replace(word, 2, "are");
+    }
+
+    std::cout << text << std::endl;
+}
+
+int main() {
+
+    replace_dummy();
+    replace_is();
+
 
     return 0;
 }
