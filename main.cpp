a86b43b56688bfddace376a8604038a1ec779ddf
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-20 15:43:25 +0400

    Calculator with pointer

--- main.cpp
+++ main.cpp
@@ -1,62 +1,44 @@
 #include "iostream"
 
-int init_params();
-
-int test(std::string type, int num1, int num2);
-
-int main() {
-
-    int i = init_params();
-    std::cout << i;
-
-    return 0;
-}
-
-int init_params() {
-
-    int num1 = 0;
-    int num2 = 0;
-    std::string type;
+int init_params(std::string *type, int *num1, int *num2) {
 
     std::cout << "Input num 1: ";
-    std::cin >> num1;
+    std::cin >> *num1;
     std::cout << "Input num 2: ";
-    std::cin >> num2;
+    std::cin >> *num2;
     std::cout << "Input type: ";
-    std::cin >> type;
-
-
-    return test(type, num1, num2);
+    std::cin >> *type;
 }
-int test(std::string type, int num1, int num2) {
-    if (type == "*") {
-        std::cout << num1 * num2 << "\n";
+
+void test (std::string *type, int *num1, int *num2) {
+
+    if (*type == "*") {
+        std::cout << *num1 * *num2 << "\n";
     }
-    else if (type == "+") {
-        std::cout << num1 + num2 << "\n";
+    else if (*type == "+") {
+        std::cout << *num1 + *num2 << "\n";
     }
-    else if (type == "-") {
-        std::cout << num1 - num2 << "\n";
+    else if (*type == "-") {
+        std::cout << *num1 - *num2 << "\n";
     }
-    else if (type == "/") {
-        std::cout << num1 / num2 << "\n";
+    else if (*type == "/") {
+        std::cout << *num1 / *num2 << "\n";
     }
     else {
         std::cout << "Something wrong, pls try again." << "\n";
     }
+}
 
-    std::string yes;
-    std::cout << "Do you want try again? y/n" << "\n";
-    std::cin >> yes;
+int main() {
 
-    if(yes == "y"){
-        return init_params();
-    } else if (yes == "n"){
-        std::cout << "Thank you!\n";
-    } else {
-        std::cout << "Something wrong, pls try again. \n";
-        return init_params();
-    }
+    int n1;
+    int n2;
+    std::string t;
+
+    init_params(&t, &n1, &n2);
+    test(&t, &n1, &n2);
+
 
     return 0;
 }
+
