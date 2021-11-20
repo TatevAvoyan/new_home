928e2a70f33f700fec743e94be13efea7f39ed88
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-20 13:47:08 +0400

    commit

--- cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/tatev/Desktop/HomeWork/untitled/main.cpp" "/home/tatev/Desktop/HomeWork/untitled/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
+  "/home/tatev/Desktop/HomeWork/CPP/untitled/main.cpp" "/home/tatev/Desktop/HomeWork/CPP/untitled/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tatev/Desktop/HomeWork/untitled
+# Install script for directory: /home/tatev/Desktop/HomeWork/CPP/untitled
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tatev/Desktop/HomeWork/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tatev/Desktop/HomeWork/CPP/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
