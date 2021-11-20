9adb773083c36310fb861c89c97a041db2b1b722
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-20 15:29:01 +0400

    Calculator

--- main.cpp
+++ main.cpp
@@ -1,48 +1,62 @@
 #include "iostream"
-#include "string"
 
+int init_params();
 
-void replace_dummy(){
-
-    std::cout << "Enter the text.\n";
-    std::string text;
-    std::getline(std::cin, text);
-    auto word = text.find("dummy");
-
-    std::cout << word << "\n";
-
-    if (std::string::npos == word ) {
-        std::cout << "not found\n";
-    } else {
-        text.replace(word + 4, 1, "i");
-    }
-
-    std::cout << text << std::endl;
-}
-
-void replace_is(){
-
-    std::cout << "Enter the text.\n";
-    std::string text;
-    std::getline(std::cin, text);
-    auto word = text.find("is");
-
-    std::cout << word << "\n";
-
-    if (std::string::npos == word ) {
-        std::cout << "not found\n";
-    } else {
-        text.replace(word, 2, "are");
-    }
-
-    std::cout << text << std::endl;
-}
+int test(std::string type, int num1, int num2);
 
 int main() {
 
-    replace_dummy();
-    replace_is();
+    int i = init_params();
+    std::cout << i;
 
+    return 0;
+}
+
+int init_params() {
+
+    int num1 = 0;
+    int num2 = 0;
+    std::string type;
+
+    std::cout << "Input num 1: ";
+    std::cin >> num1;
+    std::cout << "Input num 2: ";
+    std::cin >> num2;
+    std::cout << "Input type: ";
+    std::cin >> type;
+
+
+    return test(type, num1, num2);
+}
+int test(std::string type, int num1, int num2) {
+    if (type == "*") {
+        std::cout << num1 * num2 << "\n";
+    }
+    else if (type == "+") {
+        std::cout << num1 + num2 << "\n";
+    }
+    else if (type == "-") {
+        std::cout << num1 - num2 << "\n";
+    }
+    else if (type == "/") {
+        std::cout << num1 / num2 << "\n";
+    }
+    else {
+        std::cout << "Something wrong, pls try again." << "\n";
+    }
+
+    std::string yes;
+    std::cout << "Do you want try again? y/n" << "\n";
+    std::cin >> yes;
+
+    if(yes == "y"){
+        return init_params();
+    } else if (yes == "n"){
+        std::cout << "Thank you!\n";
+    } else {
+        std::cout << "Something wrong, pls try again. \n";
+        return init_params();
+    }
 
     return 0;
 }
